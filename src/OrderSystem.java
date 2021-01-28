cc08872d706391e26bc1a564bb8882a4a4646a9d
stapler <stapler@rose-hulman.edu>
2021-01-27 19:27:36 -0500

    milestone2

--- src/OrderSystem.java
+++ src/OrderSystem.java
@@ -1,15 +1,33 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONArray;
 
 public class OrderSystem implements SystemSubject{
 	ArrayList<Order> orders;
 	
 	public OrderSystem() throws Exception{
-		Object obj = new JSONParser().parse(new FileReader("order-input.json"));
-		JSONObject jo = (JSONObject) obj;
+	    ArrayList<JSONObject> json = new ArrayList<JSONObject>();
+	    JSONObject obj;
+	    
+	    String fileName = "order-input.json";
+
+	    String line = null;
+	    int x = 0;
+
+	    FileReader fileReader = new FileReader(fileName);
+	    BufferedReader bufferedReader = new BufferedReader(fileReader);
+	    while((line = bufferedReader.readLine()) != null) {
+	    	System.out.println(x++);
+//	    	json.add(obj);
+	    }
+	    
+	    bufferedReader.close();         
+	    
+
 		
 	}
 
